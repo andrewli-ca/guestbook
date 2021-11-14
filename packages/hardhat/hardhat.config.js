118da2c7f542a880af2fb3c9116b95f927b7f368
Andrew Li <mail@andrewli.ca>
2021-11-14 03:36:28 -0800

    Fix local node errors

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 module.exports = {
   solidity: '0.8.4',
   networks: {
+    hardhat: {
+      gas: 2100000,
+      gasPrice: 8000000000,
+      chainId: 1337, // https://hardhat.org/metamask-issue.html
+    },
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.RINKEBY_PRIVATE_KEY],

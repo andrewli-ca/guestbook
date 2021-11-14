d97ded26a7396df58a2abe9ea56cf5bafcf88118
Andrew Li <mail@andrewli.ca>
2021-11-13 16:11:49 -0800

    Constants

---
+++ packages/next-app/utils/constants.js
@@ -0,0 +1,3 @@
+export const TWITTER_URL = 'https://twitter.com/andrewli_ca';
+export const ETHERSCAN_BASE_URL = 'https://rinkeby.etherscan.io/address';
+export const CONTRACT_ADDRESS = '0x2D41192b4208344187318a785D5e7cAb686683A4';

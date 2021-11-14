5f385dd99241bf778c8a5b6d4266c0b8a0525c40
Andrew Li <mail@andrewli.ca>
2021-11-13 19:19:19 -0800

    Setup new message event

--- packages/hardhat/contracts/MessagePortal.sol
+++ packages/hardhat/contracts/MessagePortal.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.4;
 import 'hardhat/console.sol';
 
 contract MessagePortal {
+  event NewMessage(address indexed from, uint256 timestamp, string message);
+
   struct Message {
     address visitor;
     string message;
@@ -15,6 +17,7 @@ contract MessagePortal {
 
   function send(string memory _message) public {
     messages.push(Message(msg.sender, _message, block.timestamp));
+    emit NewMessage(msg.sender, block.timestamp, _message);
   }
 
   function getAllMessages() public view returns (Message[] memory) {
